0c6972c860fdfb9c0c0cdfa6bd22a8a519e82b86
洪锐 李 <kouruiri@89.41.167.220.broad.cd.sc.dynamic.163data.com.cn>
2014-03-20 14:11:38 +0800

    Initial Commit

---
+++ opencv test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencv test
+//
+//  Created by 洪锐 李 on 14-3-20.
+//  Copyright (c) 2014年 洪锐 李. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
